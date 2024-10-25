9a7e8447cbad69a4022b79955256a3edc615f261
TechnoVar197 <mr.aniruddha.ponnuri.197@gmail.com>
2024-10-25 20:43:05 +0530

    Revert "Overlay button"
    This reverts commit 799d2d93993a3e420fa433b8b64030847a3d7c2d.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Chatbot from './Chatbot';
-import './App.css'; 
-import logo from './logo.png'; 
-import chat from './chatbot.png';
+import './App.css'; // Import global CSS
+import logo from './logo.svg'; // Update the path accordingly
 
 const App = () => {
-  const [showChatbot, setShowChatbot] = useState(false);
-
-  const toggleChatbot = () => {
-    setShowChatbot(!showChatbot);
-  };
-
   return (
     <Router>
       <div className="app-container">
         {/* Title bar */}
         <div className="titlebar">
           <img src={logo} alt="Logo" className="logo" />
+          <h1 className="title"> </h1>
         </div>
 
-        {/* Right Panel */}
+        {/* Chatbot with PDF upload functionality integrated */}
         <div className="right-panel">
-          {/* Chatbot Overlay */}
-          {showChatbot && (
-            <div className="chatbot-overlay">
-              <Chatbot />
-            </div>
-          )}
-
-          {/* Chat Icon */}
-          <div className="chat-icon" onClick={toggleChatbot}>
-            <img src={chat} alt="Chat Icon" />
-          </div>
+          <Chatbot />
         </div>
       </div>
 
